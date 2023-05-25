cf9ceb5fed05116b375d68aeebf40d3005943469
lucasmonstro <joao.galiano.silva@gmail.com>
2023-05-25 18:18:47 +0100

    feat: enable api versioning

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { VersioningType } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import {
   FastifyAdapter,
@@ -10,6 +11,7 @@ async function bootstrap() {
     AppModule,
     new FastifyAdapter(),
   );
+  app.enableVersioning({ type: VersioningType.URI });
   await app.listen(3000);
 }
 bootstrap();
