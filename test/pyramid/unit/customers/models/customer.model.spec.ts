84710bbf7ce3814f8cbcb52ffc8527382bfbe944
lucasmonstro <joao.galiano.silva@gmail.com>
2023-05-26 20:41:27 +0100

    test: add unit tests to customer model

---
+++ test/pyramid/unit/customers/models/customer.model.spec.ts
@@ -0,0 +1,18 @@
+import { makeCustomer } from '../../../../mocks/customers/models/customer.model';
+
+describe('Customer', () => {
+  it('should get correctly cache key', async () => {
+    const mockedCustomer = makeCustomer();
+    const cacheKey = `customer:${mockedCustomer.id}`;
+    expect(mockedCustomer.getCacheKey()).toBe(cacheKey);
+  });
+
+  it('should get correctly fields to be cached', async () => {
+    const mockedCustomer = makeCustomer();
+    expect(mockedCustomer.toCache()).toStrictEqual({
+      id: mockedCustomer.id,
+      name: mockedCustomer.name,
+      document: mockedCustomer.document,
+    });
+  });
+});
