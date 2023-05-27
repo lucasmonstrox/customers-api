98754c0d45ea6e4c794c6e8c18fa5b3cc3262cf2
lucasmonstro <joao.galiano.silva@gmail.com>
2023-05-27 03:16:23 +0100

    test: add unit tests to update-customer service

---
+++ test/pyramid/unit/customers/services/update-customer.service.spec.ts
@@ -0,0 +1,45 @@
+import { Test } from '@nestjs/testing';
+import { CustomerDto } from '../../../../../src/customers/dto';
+import { Customer } from '../../../../../src/customers/models';
+import { SaveCustomerRepository } from '../../../../../src/customers/repositories';
+import { UpdateCustomerService } from '../../../../../src/customers/services';
+import { makeCustomerDto } from '../../../../mocks/customers/dto/customer.dto';
+import { makeCustomer } from '../../../../mocks/customers/models/customer.model';
+
+describe('UpdateCustomerService', () => {
+  let saveCustomerRepository: SaveCustomerRepository;
+  let updateCustomerService: UpdateCustomerService;
+
+  beforeEach(async () => {
+    const moduleRef = await Test.createTestingModule({
+      providers: [SaveCustomerRepository, UpdateCustomerService],
+    })
+      .overrideProvider(SaveCustomerRepository)
+      .useValue({ execute: jest.fn() })
+      .compile();
+    saveCustomerRepository = moduleRef.get<SaveCustomerRepository>(
+      SaveCustomerRepository,
+    );
+    updateCustomerService = moduleRef.get<UpdateCustomerService>(
+      UpdateCustomerService,
+    );
+  });
+
+  it('should save new Customer', async () => {
+    const mockedCustomer = makeCustomer();
+    const customerDto = makeCustomerDto();
+    const updatedCustomer = Object.assign<Customer, CustomerDto>(
+      mockedCustomer,
+      customerDto,
+    );
+    const saveCustomerRepositorySpy = jest
+      .spyOn(saveCustomerRepository, 'execute')
+      .mockResolvedValueOnce(undefined);
+    const result = await updateCustomerService.execute(
+      mockedCustomer,
+      customerDto,
+    );
+    expect(result).toStrictEqual(updatedCustomer);
+    expect(saveCustomerRepositorySpy).toHaveBeenCalledWith(updatedCustomer);
+  });
+});
