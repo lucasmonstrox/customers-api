0dd43fd190c73ba33ed9d9b5a73386c045d0d398
lucasmonstro <joao.galiano.silva@gmail.com>
2023-05-27 03:10:35 +0100

    test: add unit tests to save-customer service

---
+++ test/pyramid/unit/customers/services/save-customer.service.spec.ts
@@ -0,0 +1,32 @@
+import { Test } from '@nestjs/testing';
+import { SaveCustomerRepository } from '../../../../../src/customers/repositories';
+import { SaveCustomerService } from '../../../../../src/customers/services';
+import { makeCustomerDto } from '../../../../mocks/customers/dto/customer.dto';
+
+describe('SaveCustomerService', () => {
+  let saveCustomerRepository: SaveCustomerRepository;
+  let saveCustomerService: SaveCustomerService;
+
+  beforeEach(async () => {
+    const moduleRef = await Test.createTestingModule({
+      providers: [SaveCustomerRepository, SaveCustomerService],
+    })
+      .overrideProvider(SaveCustomerRepository)
+      .useValue({ execute: jest.fn() })
+      .compile();
+    saveCustomerRepository = moduleRef.get<SaveCustomerRepository>(
+      SaveCustomerRepository,
+    );
+    saveCustomerService =
+      moduleRef.get<SaveCustomerService>(SaveCustomerService);
+  });
+
+  it('should save new Customer', async () => {
+    const customerDto = makeCustomerDto();
+    const saveCustomerRepositorySpy = jest
+      .spyOn(saveCustomerRepository, 'execute')
+      .mockResolvedValueOnce(undefined);
+    await saveCustomerService.execute(customerDto);
+    expect(saveCustomerRepositorySpy).toHaveBeenCalled();
+  });
+});
