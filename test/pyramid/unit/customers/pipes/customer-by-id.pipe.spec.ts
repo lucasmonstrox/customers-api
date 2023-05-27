f02b6689d4c86aad07137e7302fbc8733a9a9db6
lucasmonstro <joao.galiano.silva@gmail.com>
2023-05-27 02:46:12 +0100

    test: add customer-by-id unit tests

---
+++ test/pyramid/unit/customers/pipes/customer-by-id.pipe.spec.ts
@@ -0,0 +1,42 @@
+import { faker } from '@faker-js/faker';
+import { Test } from '@nestjs/testing';
+import { CustomerNotFoundException } from '../../../../../src/customers/exceptions';
+import { CustomerByIdPipe } from '../../../../../src/customers/pipes';
+import { GetCustomerRepository } from '../../../../../src/customers/repositories';
+import { makeCustomer } from '../../../../mocks/customers/models/customer.model';
+
+describe('CustomerByIdPipe', () => {
+  let customerByIdPipe: CustomerByIdPipe;
+  let getCustomerRepository: GetCustomerRepository;
+
+  beforeEach(async () => {
+    const moduleRef = await Test.createTestingModule({
+      controllers: [CustomerByIdPipe],
+      providers: [GetCustomerRepository],
+    })
+      .overrideProvider(GetCustomerRepository)
+      .useValue({ execute: jest.fn() })
+      .compile();
+    customerByIdPipe = moduleRef.get<CustomerByIdPipe>(CustomerByIdPipe);
+    getCustomerRepository = moduleRef.get<GetCustomerRepository>(
+      GetCustomerRepository,
+    );
+  });
+
+  it('should throw CustomerNotFoundException when customer is null', async () => {
+    jest.spyOn(getCustomerRepository, 'execute').mockResolvedValueOnce(null);
+    const customerId = faker.string.uuid();
+    await expect(customerByIdPipe.transform(customerId)).rejects.toThrow(
+      CustomerNotFoundException,
+    );
+  });
+
+  it('should return Customer when Customer is found', async () => {
+    const mockedCustomer = makeCustomer();
+    jest
+      .spyOn(getCustomerRepository, 'execute')
+      .mockResolvedValueOnce(mockedCustomer);
+    const result = await customerByIdPipe.transform(mockedCustomer.id);
+    expect(result).toStrictEqual(mockedCustomer);
+  });
+});
