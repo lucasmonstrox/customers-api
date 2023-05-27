c4db1069df99fa70524b3d0db6efeafceaf2ce22
lucasmonstro <joao.galiano.silva@gmail.com>
2023-05-27 17:05:56 +0100

    refactor: set redis config through ConfigService

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { RouterModule } from '@nestjs/core';
 import { CacheModule } from './cache/cache.module';
 import { CustomerModule } from './customers/customer.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
     CacheModule,
     CustomerModule,
     RouterModule.register([
--- src/cache/factories/cache.provider.ts
+++ src/cache/factories/cache.provider.ts
@@ -1,9 +1,14 @@
+import { Provider } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import Redis from 'ioredis';
 
-export const cacheFactory = {
+export const cacheFactory: Provider = {
   provide: 'CACHE',
-  useFactory: () => {
-    // TODO: add redis config
-    return new Redis();
+  useFactory: (configService: ConfigService): Redis => {
+    return new Redis(
+      configService.get('REDIS_PORT'),
+      configService.get('REDIS_HOST'),
+    );
   },
+  inject: [ConfigService],
 };
