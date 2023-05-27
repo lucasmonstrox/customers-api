20003718a01e65b1dc8598306d9de04921dac447
lucasmonstro <joao.galiano.silva@gmail.com>
2023-05-27 17:21:50 +0100

    refactor: add missing type

--- test/mocks/customers/dto/customer.dto.ts
+++ test/mocks/customers/dto/customer.dto.ts
@@ -1,7 +1,7 @@
 import { faker } from '@faker-js/faker';
 import { CustomerDto } from '../../../../src/customers/dto';
 
-export const makeCustomerDto = (data?: Partial<CustomerDto>) => ({
+export const makeCustomerDto = (data?: Partial<CustomerDto>): CustomerDto => ({
   name: faker.person.fullName(),
   document: faker.string.uuid(),
   ...data,
